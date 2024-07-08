0c98a8f2f00d1acdd79c21731e65e516c5a1c839
jawadrada <jawadrada18@gmail.com>
2024-07-08 00:12:30 -0400

    Initalized Project Structure

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto text-5xl text-blue-500 text-center p-5">
+        <h1>Lingo Verse</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
+
+
