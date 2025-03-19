b1b87f79cdcee96902e88ae844c341d323a7b1a0
jawadrada <jawadrada18@gmail.com>
2025-03-19 17:57:59 -0400

    Remove Test api from views

--- djangoapi/translation_app/urls.py
+++ djangoapi/translation_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import test_openai, translate_text
+from .views import translate_text
 
 urlpatterns = [
-    path('test-openai/', test_openai, name='test_openai'),
     path('translate/', translate_text, name='translate_text'),
 ]
