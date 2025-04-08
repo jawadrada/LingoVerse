f0c5d3789ec52af1191bb04d6300cb62b61ede24
jawadrada <jawadrada18@gmail.com>
2025-04-08 17:33:11 -0400

    Add useTranslation Hook

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,11 +1,14 @@
 import "./Footer.css";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { useTranslation } from 'react-i18next';
 
 function Footer() {
+    const { t, i18n } = useTranslation();
+
   return (
     <footer>
         <p className="footer-text">
-            &copy; 2025 <a href="/"><strong>LingoVerse</strong></a>. All rights reserved.
+            &copy; 2025 <a href="/"><strong>LingoVerse</strong></a>. {t("footer_text")}
         </p>
         <div className="footer-icons">
             <a href="/">
