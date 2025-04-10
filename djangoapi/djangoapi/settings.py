c20664199ef22a82798b982ac54f214ba5c08bc5
jawadrada <jawadrada18@gmail.com>
2025-04-10 19:11:00 -0400

    Add db setting changes

--- djangoapi/djangoapi/settings.py
+++ djangoapi/djangoapi/settings.py
@@ -90,12 +90,24 @@ WSGI_APPLICATION = "djangoapi.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lingo_verse',
+        'USER': 'lingo_verse_user',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
