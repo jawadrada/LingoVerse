8813089041548226e834866531c82cd1d1c13fc7
jawadrada <jawadrada18@gmail.com>
2025-04-10 19:08:30 -0400

    Update translation page

--- client/src/pages/Translation/Translation.tsx
+++ client/src/pages/Translation/Translation.tsx
@@ -1,10 +1,12 @@
 import "./Translation.css";
 import LanguageTranslation from "../../components/LanguageTranslation/LanguageTranslation.tsx";
+import CommonTranslations from "../../components/CommonTranslations/CommonTranslations.tsx";
 
 function Translation() {
   return (
     <>
       <LanguageTranslation />
+      <CommonTranslations />
     </>
   );
 }
