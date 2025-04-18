a552a943861b1b740a2536e45c9d8ee791d4290b
jawadrada <jawadrada18@gmail.com>
2025-04-18 01:18:35 -0400

    Update Button component

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -3,35 +3,44 @@ import { useNavigate } from "react-router-dom";
 import { ReactNode } from "react";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    children: ReactNode;
-    route?: string;
-    mode?: "default" | "outline" | "text";
-    extraCssClass?: string,
+  children: ReactNode;
+  route?: string;
+  mode?: "default" | "outline" | "text";
+  extraCssClass?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, route, onClick, extraCssClass, mode = "default", ...props }) => {
-    const navigate = useNavigate();
-    let cssClasses = `btn ${mode}-btn ${mode}-btn:hover `;
+const Button: React.FC<ButtonProps> = ({
+  children,
+  route,
+  onClick,
+  extraCssClass,
+  mode = "default",
+  ...props
+}) => {
+  const navigate = useNavigate();
+  let cssClasses = `btn ${mode}-btn ${mode}-btn:hover `;
 
-    if(extraCssClass !== "") {
-        cssClasses += extraCssClass
+  if (extraCssClass !== "") {
+    cssClasses += extraCssClass;
+  }
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    if (onClick) {
+      onClick(event);
     }
 
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        if (onClick) {
-            onClick(event);
-        }
-
-        if (route) {
-            navigate(route);
-        }
+    if (route) {
+      navigate(route);
     }
+  };
 
-    return (
-        <button className={cssClasses} onClick={handleClick} {...props}>
-            {children}
-        </button>
-    );
-}
+  return (
+    <button className={cssClasses} onClick={handleClick} {...props}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
