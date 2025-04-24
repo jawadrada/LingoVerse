7240ea3c7b76ada35f0fa1b0e6787daa45f3680c
jawadrada <jawadrada18@gmail.com>
2025-04-23 23:18:54 -0400

    chore: Update home page UI

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
-import "./Home.css"
+import "./Home.css";
 import Button from "../../components/Button/Button.tsx";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 function Home() {
   const { t } = useTranslation();
@@ -8,17 +8,25 @@ function Home() {
   return (
     <div className="home-container">
       <div className="home-flex-container">
-        <h1>Lingo Verse</h1>
-        <p>{t('app_description')}</p>
+        <h1>Arabic Dialect Translations</h1>
+        <p>
+          {t("app_description")}
+        </p>
 
         <div className="home-row-container">
-          {/*Replace with flashcard gif*/}
-          <img className="home-image-container" src="/demo.gif" alt="GIF Showing translations" />
-          <img className="home-image-container" src="/demo.gif" alt="GIF Showing translations" />
+          <div className="home-image-card">
+            <img
+              className="home-image-container"
+              src="/demo.gif"
+              alt="GIF Showing translations"
+            />
+          </div>
         </div>
-        
+
         <div className="home-btn-container">
-          <Button route="/translation">{t('home_page_button')}</Button>
+          <Button route="/translation">
+            {t("home_page_button")}
+          </Button>
         </div>
       </div>
     </div>
