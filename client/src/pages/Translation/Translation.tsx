fd2085dfe038b438690c21bfaf2dfcf8512d779f
jawadrada <jawadrada18@gmail.com>
2025-04-23 20:11:49 -0400

    chore: moved learning content to learning page

--- client/src/pages/Translation/Translation.tsx
+++ client/src/pages/Translation/Translation.tsx
@@ -1,12 +1,10 @@
 import "./Translation.css";
 import LanguageTranslation from "../../components/LanguageTranslation/LanguageTranslation.tsx";
-import CommonTranslations from "../../components/CommonTranslations/CommonTranslations.tsx";
 
 function Translation() {
   return (
     <>
       <LanguageTranslation />
-      <CommonTranslations />
     </>
   );
 }
